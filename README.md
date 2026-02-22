a11dd373397daec4bfbc83424fb14eb04b408e9a
Maifariza <maifarizadyas@gmail.com>
2026-02-22 11:45:46 +0800

    flutter_pab_1

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_pab_1
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+// main.dart
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'models/cart_model.dart';
+import 'pages/product_list_page.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CartModel(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shopping Cart',
+      theme: ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.pinkAccent,
+    ),
+    scaffoldBackgroundColor: Colors.pink.shade50,
+  ),
+      home: const ProductListPage(),
+    );
+  }
+}
